d50bf0bad69a5cfea3796f00a1e5abe040b74c39
Héliton Nordt <hnordt@hnordt.com>
2016-04-15 12:51:36 -0300

    Cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,57 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import {observable} from 'mobx';
-import {observer} from 'mobx-react';
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
-const appState =  new class AppState {
-    @observable timer = 0;
-    
-    constructor() {
-        setInterval(() => {
-            appState.timer += 1;
-        }, 1000);
-    }
-    
-    resetTimer() {
-        this.timer = 0;
-    }
-}();
+class TimerStore {
+  @observable secondsElapsed;
+
+  constructor(secondsElapsed = 0) {
+    this.secondsElapsed = secondsElapsed;
+  }
+
+  tick = () => {
+    this.secondsElapsed += 1;
+  }
+
+  reset = () => {
+    this.secondsElapsed = 0;
+  }
+}
 
 @observer
-class TimerView extends Component {
-     render() {
-        return (
-            <div>
-                <button onClick={this.onReset}>
-                    Seconds passed: {this.props.appState.timer}
-                </button>
-                <DevTools />
-            </div>
-        );
-     }
+class Timer extends Component {
+  render() {
+    const { secondsElapsed, reset } = this.props.store
+    return (
+      <div>
+        <button onClick={reset}>
+          Seconds elapsed: {secondsElapsed}
+        </button>
+        <DevTools />
+      </div>
+    );
+  }
 
-     onReset = () => {
-     	this.props.appState.resetTimer();
-     }
+  componentDidMount = () => {
+    const { tick } = this.props.store
+    this.interval = setInterval(tick, 1000);
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.interval);
+  }
+
+  static propTypes = {
+    store: PropTypes.shape({
+      secondsElapsed: PropTypes.number.isRequired,
+      tick: PropTypes.func.isRequired,
+      reset: PropTypes.func.isRequired
+    }).isRequired
+  }
 };
 
-ReactDOM.render(<TimerView appState={appState} />, document.getElementById('root'));
+const timerStore = new TimerStore()
+
+render(<Timer store={timerStore} />, document.getElementById('root'));
